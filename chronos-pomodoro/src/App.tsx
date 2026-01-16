235f27206531444b06cbefdbc05d1c0512ca7f88
Guilherme Machado Neves <gui.m.neves01@gmail.com>
2026-01-16 15:52:12 -0100

    style input Ativo/inativo

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -29,6 +29,7 @@ export function App(){
               type='text' 
               id='input' 
               labelText='Task'
+              placeholder='Digite Algo'
             />
           </div>
 
--- chronos-pomodoro/src/components/DefaultInput/index.tsx
+++ chronos-pomodoro/src/components/DefaultInput/index.tsx
@@ -1,3 +1,5 @@
+import styles from './styles.module.css';
+
 type DefaultInpitProps = {
     id: string;
     labelText?: string;
@@ -9,7 +11,7 @@ export function DefaultInput({ id, type, labelText, ...rest }: DefaultInpitProps
             {/* {if ? 'verdadeiro' : 'Falso'} */}
 
             {labelText && <label htmlFor={id}>{ labelText }</label>}
-            <input id={id} type={type} {...rest} />
+            <input className={styles.input} id={id} type={type} {...rest} />
         </>
     );
 }

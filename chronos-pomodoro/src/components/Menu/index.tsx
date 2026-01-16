54a82fac52597bf53943af37a479f4a1ae41db50
Guilherme Machado Neves <gui.m.neves01@gmail.com>
2026-01-16 18:15:53 -0100

    capturando evento de click com preventDefault para tema dark e light

--- chronos-pomodoro/src/components/Menu/index.tsx
+++ chronos-pomodoro/src/components/Menu/index.tsx
@@ -1,19 +1,32 @@
-import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react'
-import styles from './Style.module.css'
+import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
+import styles from './Style.module.css';
+
+import { useState } from 'react';
+
+type AvailableTheme = 'dark' | 'light';
 
 export function Menu(){
+    const [ theme, setTheme] = useState<AvailableTheme>('dark');
+
+
+    function handleThemeChange(e: React.MouseEvent<HTMLAnchorElement, MouseEvent>){
+        e.preventDefault();
+        console.log('Clicado', Date.now())
+    }
+
     return (
     <nav className={styles.menu}>
-        <a href="#" className={styles.menuLink}>
+        <h1>{theme}</h1>
+        <a href="#" className={styles.menuLink} aria-label='Home' title='Home'>
             <HouseIcon />
         </a>
-        <a href="#" className={styles.menuLink}>
+        <a href="#" className={styles.menuLink} aria-label='Historico' title='Historico'>
             <HistoryIcon />
         </a>
-        <a href="#" className={styles.menuLink}>
+        <a href="#" className={styles.menuLink} aria-label='Configurações' title='Configurações'>
             <SettingsIcon />
         </a>
-        <a href="#" className={styles.menuLink}>
+        <a href="#" className={styles.menuLink} aria-label='Tema' title='Tema' onClick={handleThemeChange}>
             <SunIcon />
         </a>
     </nav>
